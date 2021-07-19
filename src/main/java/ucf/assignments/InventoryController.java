30ec0db0118ce22d374efd55ed41a34f6f82fddf
William <ZhengWilliam@knights.ucf.edu>
2021-07-19 17:27:37 -0400

    Added @FXML variables

--- src/main/java/ucf/assignments/InventoryController.java
+++ src/main/java/ucf/assignments/InventoryController.java
@@ -6,17 +6,62 @@ package ucf.assignments;
  */
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class InventoryController implements Initializable {
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+    @FXML
+    private TextField ItemName;
 
+    @FXML
+    private TextField ItemSerialNumber;
+
+    @FXML
+    private TextField ItemValue;
+
+    @FXML
+    private TableView<InventoryItem> myToDoTable;
+
+    @FXML
+    private TableColumn<InventoryItem, String> InventoryItemSerialNumberColumn;
+
+    @FXML
+    private TableColumn<InventoryItem, String> InventoryItemNameColumn;
+
+    @FXML
+    private TableColumn<InventoryItem, Integer> InventoryItemValueColumn;
+
+    @FXML
+    private Button AddEventButton;
+
+    @FXML
+    private Button ModifyEventButton;
+
+    @FXML
     public void ImportFileButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void ExportFileButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void AddItemButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void EditItemButtonClicked(ActionEvent actionEvent) {
     }
 }
